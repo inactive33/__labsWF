69f6977bdcb1acbfb468009872c9eb8d29016f16
inactive33 <325vi7001@gmail.com>
2024-09-06 00:39:51 +0300

    Добавьте файлы проекта.

---
+++ __lab13/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace __lab13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.richTxtBox_emailMessage = new System.Windows.Forms.RichTextBox();
+            this.btn_OpenEmail = new System.Windows.Forms.Button();
+            this.richTxtBox_Email = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(246, 295);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 40);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(724, 404);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(64, 34);
+            this.btn_exit.TabIndex = 2;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "txt";
+            this.saveFileDialog1.Filter = "*.txt|";
+            this.saveFileDialog1.InitialDirectory = "C:\\";
+            // 
+            // richTxtBox_emailMessage
+            // 
+            this.richTxtBox_emailMessage.Location = new System.Drawing.Point(246, 89);
+            this.richTxtBox_emailMessage.Name = "richTxtBox_emailMessage";
+            this.richTxtBox_emailMessage.Size = new System.Drawing.Size(320, 186);
+            this.richTxtBox_emailMessage.TabIndex = 3;
+            this.richTxtBox_emailMessage.Text = "";
+            // 
+            // btn_OpenEmail
+            // 
+            this.btn_OpenEmail.Location = new System.Drawing.Point(466, 295);
+            this.btn_OpenEmail.Name = "btn_OpenEmail";
+            this.btn_OpenEmail.Size = new System.Drawing.Size(100, 40);
+            this.btn_OpenEmail.TabIndex = 4;
+            this.btn_OpenEmail.Text = "Open";
+            this.btn_OpenEmail.UseVisualStyleBackColor = true;
+            this.btn_OpenEmail.Click += new System.EventHandler(this.btn_OpenEmail_Click);
+            // 
+            // richTxtBox_Email
+            // 
+            this.richTxtBox_Email.Location = new System.Drawing.Point(246, 61);
+            this.richTxtBox_Email.Name = "richTxtBox_Email";
+            this.richTxtBox_Email.Size = new System.Drawing.Size(320, 22);
+            this.richTxtBox_Email.TabIndex = 5;
+            this.richTxtBox_Email.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTxtBox_Email);
+            this.Controls.Add(this.btn_OpenEmail);
+            this.Controls.Add(this.richTxtBox_emailMessage);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "lab13";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RichTextBox richTxtBox_emailMessage;
+        private System.Windows.Forms.Button btn_OpenEmail;
+        private System.Windows.Forms.RichTextBox richTxtBox_Email;
+    }
+}
+
---
+++ __lab13/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace __lab13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnSave.Enabled = false;
+            richTxtBox_Email.TextChanged += new EventHandler(CheckEmptyText);
+            richTxtBox_emailMessage.TextChanged += new EventHandler(CheckEmptyText);
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string email = richTxtBox_Email.Text;
+            string text = richTxtBox_emailMessage.Text;
+
+            if (IsValidEmail(email))
+            {
+                saveFileDialog1 = new SaveFileDialog
+                {
+                    Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*",
+                    Title = "Сохранить email и сообщение",
+                    DefaultExt = "txt",
+                    FileName = "saved_email_message.txt"
+                };
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        SaveToFile(saveFileDialog1.FileName, email, text);
+                        MessageBox.Show("Данные успешно сохранены в файл!");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ошибка при сохранении файла: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ошибка: Введённый email некорректен. Пожалуйста, убедитесь, что email содержит символы '@' и '.', а также не содержит пробелов.", "Ошибка ввода", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        private void SaveToFile(string filePath, string email, string message)
+        {
+            // Сохраняем email и сообщение в две строки
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine(email);  
+                writer.WriteLine(message);   
+            }
+        }
+        private void OpenFile(string filePath) 
+        {
+            using (StreamReader reader = new StreamReader(filePath)) {
+                string email = reader.ReadLine();
+                string message = reader.ReadToEnd();
+
+                richTxtBox_Email.Text = email;
+                richTxtBox_emailMessage.Text = message;
+            }
+
+        }
+
+        private void btn_OpenEmail_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog 
+            {
+                Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*",
+                Title = "Открыть сохраненный файл"
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    OpenFile(openFileDialog1.FileName);
+                }
+                catch (Exception ex) 
+                {
+                    MessageBox.Show($"Ошибка при открытии файла: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void CheckEmptyText(object sender, EventArgs e)
+        {
+            string email = richTxtBox_Email.Text;
+            string text = richTxtBox_emailMessage.Text;
+
+            btnSave.Enabled = !string.IsNullOrWhiteSpace(email) && !string.IsNullOrEmpty(text);
+        }
+        private bool IsValidEmail(string email)
+        {
+           string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+           return Regex.IsMatch(email, emailPattern);
+        }
+    }
+}
